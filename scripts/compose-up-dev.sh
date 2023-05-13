b42dfb435f0fedf8fd4b784daffa79d54279ab64
Robin Dijoux <dr@ecomail.fr>
2023-05-13 23:33:48 +0200

    Dockerize front

---
+++ scripts/compose-up-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This script is used to compose the development environment.
+
+echo "Composing development environment..."
+
+# Compose the development environment.
+docker compose -f docker-compose.dev.yml up -d $1
